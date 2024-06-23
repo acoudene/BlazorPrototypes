d96046859e9b02cbc31d0ff7d54d53beb8b3e892
Anthony COUDENE <anthony.coudene@technidata-web.com>
2024-06-23 09:32:46 +0200

    Add bUnit test

---
+++ UsebUnit/MyBlazorTestProject/CounterCSharpTests.cs
@@ -0,0 +1,32 @@
+namespace MyBlazorTestProject
+{
+  /// <summary>
+  /// These tests are written entirely in C#.
+  /// Learn more at https://bunit.dev/docs/getting-started/writing-tests.html#creating-basic-tests-in-cs-files
+  /// </summary>
+  public class CounterCSharpTests : TestContext
+  {
+    [Fact]
+    public void CounterStartsAtZero()
+    {
+      // Arrange
+      var cut = RenderComponent<TestCounter>();
+
+      // Assert that content of the paragraph shows counter at zero
+      cut.Find("p").MarkupMatches("<p>Current count: 0</p>");
+    }
+
+    [Fact]
+    public void ClickingButtonIncrementsCounter()
+    {
+      // Arrange
+      var cut = RenderComponent<TestCounter>();
+
+      // Act - click button to increment counter
+      cut.Find("button").Click();
+
+      // Assert that the counter was incremented
+      cut.Find("p").MarkupMatches("<p>Current count: 1</p>");
+    }
+  }
+}
