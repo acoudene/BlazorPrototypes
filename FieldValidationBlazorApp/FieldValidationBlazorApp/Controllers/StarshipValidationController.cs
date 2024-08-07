3507dea27877f94779b39f1b909f73cfe98c3fb6
Anthony COUDENE <anthony.coudene@technidata-web.com>
2024-08-07 18:54:21 +0200

    Add validation for Blazor

---
+++ FieldValidationBlazorApp/FieldValidationBlazorApp/Controllers/StarshipValidationController.cs
@@ -0,0 +1,53 @@
+﻿using FieldValidationBlazorApp.Client.ViewObjects;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+
+namespace FieldValidationBlazorApp.Controllers;
+
+//[Authorize]
+[ApiController]
+[Route("[controller]")]
+public class StarshipValidationController : ControllerBase
+{
+  private readonly ILogger<StarshipValidationController> logger;
+
+  public StarshipValidationController(
+      ILogger<StarshipValidationController> logger)
+  {
+    this.logger = logger;
+  }
+
+  static readonly string[] scopeRequiredByApi = new[] { "API.Access" };
+
+  [HttpPost]
+  public async Task<IActionResult> Post(Starship model)
+  {
+    //HttpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
+
+    try
+    {
+      if (model.Classification == "Defense" &&
+          string.IsNullOrEmpty(model.Description))
+      {
+        ModelState.AddModelError(nameof(model.Description),
+            "For a 'Defense' ship " +
+            "classification, 'Description' is required.");
+      }
+      else
+      {
+        logger.LogInformation("Processing the form asynchronously");
+
+        // async ...
+
+        return Ok(ModelState);
+      }
+    }
+    catch (Exception ex)
+    {
+      logger.LogError("Validation Error: {Message}", ex.Message);
+    }
+
+    return BadRequest(ModelState);
+  }
+}
---
+++ FieldValidationBlazorApp/FieldValidationBlazorApp/Program.cs
@@ -0,0 +1,53 @@
+using FieldValidationBlazorApp.Client.Pages;
+using FieldValidationBlazorApp.Components;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddControllersWithViews()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+      options.InvalidModelStateResponseFactory = context =>
+      {
+        if (context.HttpContext.Request.Path == "/StarshipValidation")
+        {
+          return new BadRequestObjectResult(context.ModelState);
+        }
+        else
+        {
+          return new BadRequestObjectResult(
+              new ValidationProblemDetails(context.ModelState));
+        }
+      };
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+  app.UseWebAssemblyDebugging();
+}
+else
+{
+  app.UseExceptionHandler("/Error", createScopeForErrors: true);
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+app.MapDefaultControllerRoute();
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(FieldValidationBlazorApp.Client._Imports).Assembly);
+
+
+
+app.Run();
