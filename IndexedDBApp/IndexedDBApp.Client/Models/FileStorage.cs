0b30df321ba4572a537145e7d0741e16475e6f65
Anthony COUDENE <anthony.coudene@technidata-web.com>
2024-11-18 19:10:34 +0100

    Add images

---
+++ IndexedDBApp/IndexedDBApp.Client/Models/FileStorage.cs
@@ -0,0 +1,9 @@
+﻿namespace IndexedDBApp.Client.Models;
+
+public record FileStorage
+{
+  [System.ComponentModel.DataAnnotations.Key]
+  public long Id { get; set; }
+
+  public List<byte[]> Files { get; set; } = Enumerable.Empty<byte[]>().ToList();
+}
---
+++ IndexedDBApp/IndexedDBApp.Client/Models/MyDatabaseDb.cs
@@ -0,0 +1,11 @@
+﻿using Blazor.IndexedDB;
+using Microsoft.JSInterop;
+
+namespace IndexedDBApp.Client.Models;
+
+public class MyDatabaseDb : IndexedDb
+{
+  public MyDatabaseDb(IJSRuntime jSRuntime, string name, int version) : base(jSRuntime, name, version) { }
+  public IndexedSet<Person>? People { get; set; }
+  public IndexedSet<FileStorage>? Files { get; set; }
+}
