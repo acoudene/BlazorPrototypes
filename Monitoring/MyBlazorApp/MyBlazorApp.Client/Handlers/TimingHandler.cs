bed1c285b8f346fde2bb99602f9096962669ab42
Anthony COUDENE <anthony.coudene@technidata-web.com>
2025-08-01 20:42:40 +0200

    Add timer to blazor app

---
+++ Monitoring/MyBlazorApp/MyBlazorApp.Client/Handlers/TimingHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace MyBlazorApp.Client.Handlers;
+
+public class TimingHandler : DelegatingHandler
+{
+  protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+  {
+    var stopwatch = Stopwatch.StartNew();
+
+    var response = await base.SendAsync(request, cancellationToken);
+
+    stopwatch.Stop();
+    var elapsedMs = stopwatch.ElapsedMilliseconds;
+
+    Console.WriteLine($"[HTTP] {request.Method} {request.RequestUri} - {elapsedMs} ms");
+
+    return response;
+  }
+}
+
--- Monitoring/MyBlazorApp/MyBlazorApp.Client/Program.cs
+++ Monitoring/MyBlazorApp/MyBlazorApp.Client/Program.cs
@@ -1,5 +1,19 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MyBlazorApp.Client.Handlers;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddTransient<TimingHandler>();
+
+builder.Services.AddScoped(sp =>
+{
+  var handler = sp.GetRequiredService<TimingHandler>();
+  handler.InnerHandler = new HttpClientHandler();
+
+  return new HttpClient(handler)
+  {
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+  };
+});
 
 await builder.Build().RunAsync();
