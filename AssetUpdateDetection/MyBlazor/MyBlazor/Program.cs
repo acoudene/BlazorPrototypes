dd6a09cf7a974dadbf5b3cf60eb6ffe597b5aef7
Anthony COUDENE <anthony.coudene@technidata-web.com>
2025-08-02 14:14:36 +0200

    Try detecting update from wasm on assets

---
+++ AssetUpdateDetection/MyBlazor/MyBlazor/Program.cs
@@ -0,0 +1,34 @@
+using MyBlazor.Client.Pages;
+using MyBlazor.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddHttpClient<HttpClient>(client => client.BaseAddress = new Uri(@"https://localhost:7275"));
+
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(MyBlazor.Client._Imports).Assembly);
+
+app.Run();
