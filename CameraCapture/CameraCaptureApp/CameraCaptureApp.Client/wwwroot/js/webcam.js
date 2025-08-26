8f8b8fd00f43bb8cf6ad92ebbb929b4e44ee6f20
Anthony COUDENE <anthony.coudene@technidata-web.com>
2025-08-26 15:13:51 +0200

    First test

---
+++ CameraCapture/CameraCaptureApp/CameraCaptureApp.Client/wwwroot/js/webcam.js
@@ -0,0 +1,13 @@
+﻿window.captureImageFromCamera = async function () {
+  const video = document.getElementById('video');
+  const canvas = document.getElementById('canvas');
+  const context = canvas.getContext('2d');
+
+  if (!video.srcObject) {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    video.srcObject = stream;
+  }
+
+  context.drawImage(video, 0, 0, canvas.width, canvas.height);
+  return canvas.toDataURL('image/png');
+};
